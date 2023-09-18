44156ba268f88490fea1d96d28b431f25bfe08da
Michael Salas <masgcode@hotmail.com>
2023-09-18 13:39:53 -0600

    update gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-example/'
+  base: 'https://masgcode.github.io/vite-react-example/'
 })
